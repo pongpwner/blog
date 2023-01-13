a2b5018263490fdd37b8a8e10fb6ace3a478b44d
unknown <ericsshyus@gmail.com>
2023-01-12 19:36:45 -0500

    add sign in user

--- src/components/signInForm/signInForm.tsx
+++ src/components/signInForm/signInForm.tsx
@@ -1,12 +1,58 @@
 import "./signInForm.scss";
-
+import React, { useState } from "react";
 const SignInForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    if (e.target.name === "username") {
+      setUsername(e.target.value);
+    }
+    if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
+  }
+  async function postSignInInfo(event: React.SyntheticEvent): Promise<any> {
+    console.log("yeaaaaaa");
+    event.preventDefault();
+    let response = await fetch("http://localhost:5000/sign-in", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+
+    let data = await response.json();
+    console.log(data);
+    //save token to local storage
+  }
+
   return (
-    <form action="/sign-in" className="sign-in-form" method="POST">
+    <form
+      action="http://localhost:5000/sign-in"
+      className="sign-in-form"
+      method="POST"
+      onSubmit={postSignInInfo}
+    >
       <label htmlFor="username">username:</label>
-      <input type="text" id="username" name="username" />
+      <input
+        type="text"
+        id="username"
+        name="username"
+        value={username}
+        onChange={(e) => handleChange(e)}
+      />
       <label htmlFor="password">password:</label>
-      <input type="password" id="password" name="password" />
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={password}
+        onChange={(e) => handleChange(e)}
+      />
+      <button type="submit">sign in</button>
     </form>
   );
 };
